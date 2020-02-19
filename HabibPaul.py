7497c3f6265bed09947090145b9993e5adb0689b
Marcin Serwin <marcin.serwin2@gmail.com>
2020-02-19 21:26:08 +0100

    Add recognition test from Habib Paul

--- HabibPaul.py
+++ HabibPaul.py
@@ -36,5 +36,41 @@ def bruteForcePartition(graph: nx.Graph):
     return [s.pop() for s in partition]
 
 
+def areTheseTwins(graph: nx.Graph, partition: list, x: any, y: any):
+    if x is None or y is None:
+        return False
+
+    x_neighbors = set(graph.neighbors(x)) & set(partition)
+    y_neighbors = set(graph.neighbors(y)) & set(partition)
+
+    return x_neighbors == y_neighbors or (
+        x_neighbors | set([x])) == (y_neighbors | set([y]))
+
+
+def isCograph(graph: nx.Graph, partition: list):
+    partition = [None, *partition, None]
+
+    z = 1
+    while z + 1 != len(partition):
+        prec = partition[z - 1]
+        curr = partition[z]
+        succ = partition[z + 1]
+
+        if areTheseTwins(graph, partition, curr, prec):
+            z -= 1
+            del partition[z]
+        elif areTheseTwins(graph, partition, curr, succ):
+            del partition[z]
+        else:
+            z += 1
+
+    return len(partition) == 3
+
+
 if __name__ == "__main__":
-    print(bruteForcePartition(nx.read_yaml("./graphs/example.yaml")))
+    graph = nx.read_yaml("./graphs/example.yaml")
+    partition = bruteForcePartition(graph)
+    if isCograph(graph, partition):
+        print('example is cograph')
+    else:
+        print('example contains a P4')
