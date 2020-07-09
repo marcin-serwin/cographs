c76d25a22b1ee010a4385503865903a5fb176539
Marcin Serwin <marcin.serwin2@gmail.com>
2020-07-09 10:32:04 +0200

    Add simple pick test case

---
+++ cographs/test/utilities_test.py
@@ -0,0 +1,13 @@
+import unittest
+from .. import utilities
+
+
+class TestUtilities(unittest.TestCase):
+    def test_pick(self):
+        element = "elem"
+        a_set = {element}
+        self.assertEqual(utilities.pick(a_set), element)
+
+
+if __name__ == "__main__":
+    unittest.main()
