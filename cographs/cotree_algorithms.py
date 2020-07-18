5e4a6a0e69385f92f28dc7be9103b6cffd1432dc
Marcin Serwin <marcin.serwin2@gmail.com>
2020-07-18 14:33:15 +0200

    Add algorithm reconstructing cograph from cotree

--- cographs/cotree_algorithms.py
+++ cographs/cotree_algorithms.py
@@ -1,7 +1,10 @@
 # pyright: strict
+from __future__ import annotations
 from dataclasses import dataclass
+from itertools import product
 from functools import reduce
 from typing import Callable, TypeVar
+import networkx as nx
 from cographs.cotree_classes import InternalNode, LeafNode, TreeNode, VT
 
 TraversalResult = TypeVar("TraversalResult")
@@ -24,3 +27,22 @@ def traverse_cotree(root: TreeNode[VT], algorithm: CotreeAlgorithm) -> TR:
         algorithm.merge_union if root.is_union else algorithm.merge_join,
         (traverse_cotree(child, algorithm) for child in root.children),
     )
+
+
+def reconstruct_graph(cotree: TreeNode[VT]) -> nx.Graph[VT]:
+    def join_graphs(lhs: nx.Graph[VT], rhs: nx.Graph[VT]) -> nx.Graph[VT]:
+        graph = nx.union(lhs, rhs)
+        for edge in product(lhs.nodes, rhs.nodes):
+            graph.add_edge(*edge)
+        return graph
+
+    def trivial_graph(node: VT) -> nx.Graph[VT]:
+        graph: nx.Graph[VT] = nx.Graph()
+        graph.add_node(node)
+        return graph
+
+    return traverse_cotree(cotree, CotreeAlgorithm(
+        nx.union,
+        join_graphs,
+        lambda leaf: trivial_graph(leaf.node)
+    ))
