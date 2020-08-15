bd0cd0bbbdce7ac2b356d5afd8257b2315d5d666
Marcin Serwin <marcin.serwin2@gmail.com>
2020-08-15 14:58:24 +0200

    Add cotree algorithm experiments

--- experiments.py
+++ experiments.py
@@ -4,6 +4,8 @@ import random
 import networkx as nx
 import cographs.corneil_perl_stewart as cps
 import cographs.habib_paul as hp
+import cographs.cotree_algorithms as ca
+import cographs.verification_algorithms as va
 
 SAMPLE_SIZE = 100
 
@@ -23,7 +25,7 @@ NOT_COGRAPHS = {(logsize, seed): get_graph(logsize, seed)
                 for logsize in [4, 8, 10] for seed in range(SAMPLE_SIZE)}
 
 COGRAPHS = {(logsize, seed): get_cograph(logsize, seed)
-            for logsize in [4, 8, 10] for seed in range(SAMPLE_SIZE)}
+            for logsize in [2, 3, 4, 8, 10] for seed in range(SAMPLE_SIZE)}
 
 
 def test_recognition_algo(algo: Callable, name: str, positive_answer: bool):
@@ -42,6 +44,45 @@ def test_recognition_algo(algo: Callable, name: str, positive_answer: bool):
             "{:.4f}".format(max(deltas)))
 
 
+def test_cotree_algo(algo: Callable, brute: Callable, name: str):
+    print(name)
+
+    for log_graph_order in [2, 3, 4, 8, 10]:
+        deltas = [
+            1000 *
+            timeit.timeit(
+                lambda: algo(cps.compute_cotree(
+                    COGRAPHS[(log_graph_order, seed)])),
+                number=1) for seed in range(SAMPLE_SIZE)]
+        print(
+            2**log_graph_order,
+            "{:.4f}".format(sum(deltas) / SAMPLE_SIZE),
+            "{:.4f}".format(max(deltas)))
+
+    print("brute", name)
+
+    for log_graph_order in [2, 3]:
+        deltas = [
+            1000 *
+            timeit.timeit(
+                lambda: brute(
+                    COGRAPHS[(log_graph_order, seed)]),
+                number=1) for seed in range(SAMPLE_SIZE)]
+        print(
+            2**log_graph_order,
+            "{:.4f}".format(sum(deltas) / SAMPLE_SIZE),
+            "{:.4f}".format(max(deltas)))
+
+
+def verificator_to_bruteforce(algo: Callable, top_down: bool) -> Callable:
+    def bruteforce(graph: nx.Graph):
+        for i in range(graph.number_of_nodes())[::-1 if top_down else 1]:
+            if algo(graph, i):
+                break
+
+    return bruteforce
+
+
 def main():
     ALGORITHMS = [
         (lambda g: hp.check_partition(
@@ -55,6 +96,24 @@ def main():
 
     for algo, name in ALGORITHMS:
         test_recognition_algo(algo, name, False)
+
+    COTREE_ALGORITHMS = [
+        ("cliq",
+         ca.find_max_clique,
+         verificator_to_bruteforce(va.clique_of_size_exists, True)),
+        ("is",
+         ca.find_max_independent_set,
+         verificator_to_bruteforce(va.independent_set_of_size_exists, True)),
+        ("color",
+         ca.find_min_coloring,
+         verificator_to_bruteforce(va.is_graph_colorable_with, False)),
+        ("paths",
+         ca.find_min_path_cover,
+         verificator_to_bruteforce(va.can_be_covered_with, False)),
+    ]
+
+    for name, algo, brute in COTREE_ALGORITHMS:
+        test_cotree_algo(algo, brute, name)
 
 
 if __name__ == "__main__":
